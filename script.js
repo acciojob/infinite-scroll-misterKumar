8b383dd9a88038d8789c39300d6f4f025ff81e08
misterKumar <120304503+misterKumar@users.noreply.github.com>
2023-03-20 11:41:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 //your code here!
+const list = document.getElementById("list");
+const listContainer = document.querySelector(".infi-list");
 
+const loadMore = () => {
+  // Add 2 more list items to the list
+  const newItems = document.createDocumentFragment();
+  for (let i = 0; i < 2; i++) {
+    const li = document.createElement("li");
+    li.textContent = `List item ${list.children.length + i + 1}`;
+    newItems.appendChild(li);
+  }
+  list.appendChild(newItems);
+};
+
+listContainer.addEventListener("scroll", () => {
+  if (
+    listContainer.scrollTop + listContainer.clientHeight >=
+    listContainer.scrollHeight
+  ) {
+    loadMore();
+  }
+});
+
+// Initially load 10 list items
+loadMore();
