6acdf185c940f85b6dbe242fec587a79645afe88
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-06-20 17:36:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const listContainer = document.querySelector("#infi-list");
 const loadMore = () => {
   // Add 2 more list items to the list
   const newItems = document.createDocumentFragment();
-  for (let i = 0; i < 2; i++) {
+  for (let i = 0; i <10 ; i++) {
     const li = document.createElement("li");
     li.textContent = `List item ${list.children.length + i + 1}`;
     newItems.appendChild(li);
